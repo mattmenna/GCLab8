badfe7e5b34673845a62618022e587f7f43d0a14
Matthew Menna <mattmenna@gmail.com>
2017-07-05 14:25:49 -0400

    Love extract method! fixed casting of int to float and calculation of slugging and batting average.

--- src/BattingStats.java
+++ src/BattingStats.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 /**
@@ -15,35 +16,46 @@ public class BattingStats {
     public static void main(String[] args) {
 
         Scanner scnr = new Scanner(System.in);
-
-        System.out.println("Batting Statistics");
-
         OOValidator validator = new OOValidator();
-
-        System.out.println("Whats the player's name?:");
-        String pName = scnr.nextLine();
-
-        int numAtBats = validator.getInt("How many at bats?");
-        int[] bases = new int[numAtBats];
+        MyValidator stringValidator = new MyValidator();
         int numbOfHits = 0;
         int slug = 0;
 
+        System.out.println("Welcome to Batting Average Calculator!\nWhats the player's name?:");
+        String pName = scnr.nextLine();
+
+        System.out.println("0=out, 1=single, 2=double, 3=triple, 4=home run");
+
+        int numAtBats = validator.getInt("Enter number of times at bat:");
+        int[] bases = new int[numAtBats];
+
         for (int i = 0; i < bases.length; i++) {
             //System.out.println(i);
-            bases[i] = validator.getInt("Please enter the number of bases for at bat number" + (i+1));
-            if(bases[i]>= 1){
-                numbOfHits += bases[i];
-            }
+            bases[i] = validator.getInt("Result for at bat " + (i+1)+ ":");
+            numbOfHits = getNumbOfHits(bases[i], numbOfHits);
             slug += bases[i];
-
         }
 
+        float battingAvg = getBattingAvg(numAtBats, numbOfHits);
+        float sluggingPct = getSluggingPct(numAtBats, slug);
 
-        float battingAvg = (numbOfHits/numAtBats)*100;
-        float sluggingPct = (slug/numAtBats)*100;
+        System.out.printf("%s had a batting average of %.3f\n", pName, battingAvg);
+        System.out.printf("%s had a slugging percentage of %.3f", pName, sluggingPct);
 
-        System.out.printf("%s  had a batting average of %.3f\n", pName, battingAvg);
-        System.out.printf("%s  had a slugging percentage of %.3f", pName, sluggingPct);
+    }
 
+    private static float getSluggingPct(float numAtBats, float slug) {
+        return (slug / numAtBats)*100;
+    }
+
+    private static float getBattingAvg(float numAtBats, float numbOfHits) {
+        return (numbOfHits / numAtBats)*100;
+    }
+
+    private static int getNumbOfHits(int basis, int numbOfHits) {
+        if(basis >= 1){
+            numbOfHits += 1;
+        }
+        return numbOfHits;
     }
 }
