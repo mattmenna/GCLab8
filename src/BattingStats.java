2bf578c5867b1f331b82c17b4da995b108308069
Matthew Menna <mattmenna@gmail.com>
2017-07-05 13:24:51 -0400

    Initial Commit

--- src/BattingStats.java
+++ src/BattingStats.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
+
 /**
  * Created by Matt on 7/5/2017.
  */
+/*
+Calculate the batting average and slugging percentage for one or more baseball/softball players.
+First ask for the number of at bats
+For eah at bat ask the user for the number of bases earned by the batter.
+After all of the at-bats are entered, display the batting average and slugging percentage of the batter.
+ */
+
 public class BattingStats {
 
     public static void main(String[] args) {
 
-        
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("Batting Statistics");
+
+        OOValidator validator = new OOValidator();
+
+        System.out.println("Whats the player's name?:");
+        String pName = scnr.nextLine();
+
+        int numAtBats = validator.getInt("How many at bats?");
+        int[] bases = new int[numAtBats];
+
+        for (int i = 0; i < bases.length; i++) {
+            System.out.println(i);
+            bases[i] = validator.getInt("How many bases?");
+        }
 
     }
 }
